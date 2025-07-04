71c04694d0e82fd2f0183526cdfde381a78d714d
Jacob Gavin <jacobgavin89@gmail.com>
2025-07-05 00:49:07 +0200

    build: installed and configured react query

---
+++ app/_layout.tsx
@@ -0,0 +1,50 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
+import { Slot, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  return (
+    <>
+      {/* Keep the splash screen open until the assets have loaded. In the future, we should just support async font loading with a native version of font-display. */}
+      {loaded && <RootLayoutNav />}
+    </>
+  );
+}
+
+const client = new QueryClient()
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <>
+      <QueryClientProvider client={client}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Slot />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </>
+  );
+}
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const eslintPluginReactQuery = require("@tanstack/eslint-plugin-query")
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
+  eslintPluginReactQuery,
   {
     ignores: ["dist/*"],
   },
